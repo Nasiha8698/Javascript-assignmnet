3adb73f16fd89d096a235fd0445ad9905ddb051b
Nasiha8698 <102521824+Nasiha8698@users.noreply.github.com>
2022-05-16 07:30:08 +0000

    Update assignment11.js

--- assignment11.js
+++ assignment11.js
@@ -1,2 +1,11 @@
-arr =[1,2,3,4,5];
-console.log(arr.reverse());
+function reversearray{
+  var a=[3,5,7,8]
+  const len=a.length;
+  var revArr=new array;
+  var b=0;
+  for(var i=len-1;i>=0;i--){
+    revArr.push(a[i]);
+    document.write(revArr[b]+" ");
+    b++;
+  }
+}
